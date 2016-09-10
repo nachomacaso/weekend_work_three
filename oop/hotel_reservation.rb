6ee879d8e6db5bdd6f0d704281bbf195199f7bf7
Nate Macaso <nathanmacaso@gmail.com>
2016-09-10 00:06:50 -0500

    Initial commit

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -23,8 +23,11 @@ class HotelReservation
   def add_a_custom_amenity(amenity)
     @amenities << amenity
   end
-end
 
+  def change_room(new_number)
+    @room_number = new_number
+  end
+end
 
 # Write your own driver code below! Make sure your code tests the following:
 # The ability to change a room number even after a reservation has already been created
@@ -32,3 +35,61 @@ end
 # The add_a_crib method
 # The add_a_custom_amenity method
 
+puts "TESTING hotel_reservation"
+puts
+
+hotel_reservation = HotelReservation.new(customer_name: "John Smith", 
+                                         date: "9/9/2016",
+                                         room_number: 206)
+
+hotel_reservation.change_room(301)
+
+puts "New Room Number:"
+puts hotel_reservation.room_number
+puts
+
+if hotel_reservation.room_number == 301
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+hotel_reservation.add_a_fridge
+
+puts "Hotel amenities: "
+puts hotel_reservation.amenities
+puts
+
+if hotel_reservation.amenities.include?("fridge")
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+hotel_reservation.add_a_crib
+
+puts "Hotel amenities: "
+puts hotel_reservation.amenities
+puts
+
+if hotel_reservation.amenities.include?("crib")
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+hotel_reservation.add_a_custom_amenity("jacuzzi")
+
+puts "Hotel amenities: "
+puts hotel_reservation.amenities
+puts
+
+if hotel_reservation.amenities.include?("jacuzzi")
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
